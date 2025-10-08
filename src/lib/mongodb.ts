f2359e103cd62460e5883ea4c5b255bdadf80c03
manit-g <manit-g@users.noreply.github.com>
2025-10-08 18:47:27 +0530

    Fix MongoDB connection timeout

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -25,6 +25,8 @@ async function connectDB() {
   if (!cached.promise) {
     const opts = {
       bufferCommands: false,
+      serverSelectionTimeoutMS: 10000,
+      socketTimeoutMS: 45000,
     };
 
     cached.promise = mongoose.connect(MONGODB_URI!, opts).then((mongoose) => {
